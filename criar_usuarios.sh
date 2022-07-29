c2fa3e67e3ffb1bfbe9289ae8551b6b3fbbda221
Thiago <thiagodasilvasantos.tss@gmail.com>
2022-07-29 17:22:10 -0300

    Shell script para criar varios usuários no sistema baseado em linux

---
+++ criar_usuarios.sh
@@ -0,0 +1,5 @@
+useradd guest1 -m -c "descrição breve" -s "/bin/bash/ -p $(openssl passwd -1 Senha123)
+passwd -e #Força o usuário a mudar a senha 
+#openssl passwd --help - mostra as opções de algoritmo de ecriptção 
+
+
